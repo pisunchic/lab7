96d65a5fb6ab3101f4197c86854c2f701c121042
pisunchic <120672338+pisunchic@users.noreply.github.com>
2024-04-24 20:57:52 +0300

    change SaveNoteScreen

--- app/src/main/java/ui/components/screens/SaveNoteScreen.kt
+++ app/src/main/java/ui/components/screens/SaveNoteScreen.kt
@@ -6,24 +6,101 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.topic2.android.notes.R
 import com.topic2.android.notes.domain.model.ColorModel
 import com.topic2.android.notes.util.fromHex
 import com.topic2.android.notes.viewmodel.MainViewModel
 import ui.components.NoteColor
 
+
 @Composable
 fun SaveNoteScreen(viewModel: MainViewModel) {
 
 }
+
+@Preview
+@Composable
+fun SaveNoteTopAppBarPreview() {
+    SaveNoteTopAppBar(
+        isEditingMode = true,
+        onBackClick = {},
+        onSaveNoteClick = {},
+        onOpenColorPickerClick = {},
+        onDeleteNoteClick = {}
+    )
+}
+@Composable
+private fun SaveNoteTopAppBar(
+        isEditingMode: Boolean,
+        onBackClick: () -> Unit,
+        onSaveNoteClick: () -> Unit,
+        onOpenColorPickerClick: () -> Unit,
+        onDeleteNoteClick: () -> Unit
+    ) {
+    TopAppBar(
+        title = {
+            Text(
+                text = "Save Note",
+                color = MaterialTheme.colors.onPrimary
+            )
+        },
+        navigationIcon = {
+            IconButton(onClick = onBackClick) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = "Save Note Button",
+                    tint = MaterialTheme.colors.onPrimary
+                )
+            }
+        },
+        actions =  {
+         IconButton(onClick = onBackClick ) {
+             Icon(
+                 imageVector = Icons.Default.Check,
+                 contentDescription = "Save Note",
+                 tint = MaterialTheme.colors.onPrimary
+             )
+         }
+            IconButton(onClick = onOpenColorPickerClick) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_baseline_color_lens_24),
+                    contentDescription = "Open Color Picker Button",
+                    tint = MaterialTheme.colors.onPrimary
+                )
+                
+            }
+           if(isEditingMode) {
+         IconButton(onClick = onDeleteNoteClick) {
+             Icon(
+                 imageVector = Icons.Default.Delete,
+                 contentDescription = "Delete Note Button",
+                 tint = MaterialTheme.colors.onPrimary
+             )
+         } }
+        },
+
+    )
+
+    }
+
 @Composable
 private fun ColorPicker(
     colors: List<ColorModel>,
